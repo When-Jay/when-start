89f3086bd5f1b2650586bc8426d1e6de63ebdb1e
zhengwenjie <zhengwenjie@corp.netease.com>
2019-03-20 21:01:36 +0800

    新增redis项目

---
+++ reids/src/main/java/com/when/reids/ReidsApplication.java
@@ -0,0 +1,13 @@
+package com.when.reids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReidsApplication.class, args);
+	}
+
+}
